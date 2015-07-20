9cbcde68e9ce996823baaa5f089c399e10168101
Fazzani <tunisienheni@outlook.com>
2015-07-20 18:30:45 +0200

    HFA: Add Models to infrastructure

---
+++ BLS.Infrastructure/Mvc/Model/BaseModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BLS.Infrastructure.Mvc.Model
+{
+    public class BaseModel
+    {
+        public BaseModel()
+        {
+            CurrentUser = new CurrentUserModel();
+            Messages = new List<UIMessage>();
+        }
+        public CurrentUserModel CurrentUser { get; set; }
+        public object Result { get; set; }
+        public List<UIMessage> Messages { get; set; }
+    }
+
+    public class CurrentUserModel
+    {
+        public string DisplayName { get; set; }
+        public string Email { get; set; }
+    }
+
+    public class UIMessage
+    {
+        public string Title { get; set; }
+        public string Message { get; set; }
+        public MessageTypeEnum MessageType { get; set; }
+    }
+
+    public enum MessageTypeEnum
+    {
+        Info = 0,
+        Warning = 1,
+        Error = 2
+    }
+}
---
+++ BLS.Infrastructure/Mvc/Model/IModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BLS.Infrastructure.Mvc.Model
+{
+    public interface IModel<T> where T : class
+    {
+        T ToDTO();
+    }
+}
